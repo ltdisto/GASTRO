01837576742967b7229467fa57875041ae3c0990
ltdisto <lt.disto@gmail.com>
2026-09-11 18:06:28 +0700

    akun tersimpan v1.3

--- app.js
+++ app.js
@@ -30,8 +30,7 @@ function generateAdminSubmissionId(username) {
 
 /* ---------------- Konten Editor Admin (Google Sheets sebagai CMS) ---------------- */
 // PENTING: isi dengan URL Web App Apps Script yang sama seperti di ar.js
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz9Gw89cWOq7ZDy0Lf4i8ZhLFP0Q8QRCLsiluLndgG4X7oqMjrQ-rADCIm2-r9qiTr8pA/exec
-";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz9Gw89cWOq7ZDy0Lf4i8ZhLFP0Q8QRCLsiluLndgG4X7oqMjrQ-rADCIm2-r9qiTr8pA/exec";
 
 let MATERI_OVERRIDES = {}; // { "islandId_provIndex": { deskripsi, gambarUrl, posisiGambar } }
 
